c87ce1677df4f1f3efcac8b2fecf006527c56ab9
Jared Grimes <jaredlgrimes@hotmail.com>
2024-08-28 20:57:50 -0500

    added the time ago for match view

--- app/live-log/[id]/page.tsx
+++ app/live-log/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { Sprite } from "@/components/Sprite";
 import { parseBattleLog } from "@/components/battle-logs/battle-log.utils";
 import { createClient } from "@/utils/supabase/server";
+import { formatDistanceToNowStrict } from "date-fns";
 import { redirect } from "next/navigation";
 
 export default async function LiveLog({ params }: { params: { id: string } }) {
@@ -12,12 +13,14 @@ export default async function LiveLog({ params }: { params: { id: string } }) {
     return redirect("/");
   }
 
-  const battleLog = parseBattleLog(logData.log, logData.id, logData.user);
+  const battleLog = parseBattleLog(logData.log, logData.id, logData.created_at);
+  console.log(battleLog.date)
 
   return (
     <div className="flex-1 flex flex-col w-full h-full p-8 sm:max-w-lg justify-between gap-2">
       <div className="flex flex-col gap-8">
-        <div className="flex items-center justify-evenly">
+      <div className="flex flex-col items-center gap-4">
+      <div className="flex items-center justify-evenly w-full">
           <div className="flex items-center gap-2">
             {<Sprite name={battleLog.players[0].deck} />}
             <h2 className="text-xl font-semibold">{battleLog.players[0].name}</h2>
@@ -27,6 +30,8 @@ export default async function LiveLog({ params }: { params: { id: string } }) {
             <h2 className="text-xl font-semibold">{battleLog.players[1].name}</h2>
           </div>
         </div>
+        <h3 className="text-sm text-muted-foreground">{formatDistanceToNowStrict(battleLog.date)} ago</h3>
+      </div>
         <div>
           {battleLog.actions.map((action) => <div>{action.message}</div>)}
         </div>
