49672c11ea89ab7967d3c4ff78553be1e8a725b7
Jared Grimes <jaredlgrimes@hotmail.com>
2024-08-30 00:05:27 -0500

    added the winner/loser colors for card backgrounds

--- components/battle-logs/BattleLogPreview.tsx
+++ components/battle-logs/BattleLogPreview.tsx
@@ -8,21 +8,41 @@ import { formatDistanceToNowStrict } from "date-fns";
 import { BattleLog } from "./battle-log.types";
 import { Sprite } from "../archetype/Sprite";
 import Link from "next/link";
+import { useMemo } from "react";
+import { cn } from "@/lib/utils";
 
 interface BattleLogPreviewProps {
   // unparsed battle log
   battleLog: BattleLog;
+  currentUserScreenName: string | undefined;
 }
 
 export function BattleLogPreview (props: BattleLogPreviewProps) {
+  const shouldReversePlayers = useMemo(() => {
+    // because it doesn't matter
+    if (!props.currentUserScreenName) return false;
+
+    if (props.battleLog.players[1].name === props.currentUserScreenName) return true;
+
+    return false;
+  }, [props.battleLog.players, props.currentUserScreenName]);
+
+  const players = useMemo(() => {
+    if (shouldReversePlayers) return props.battleLog.players.reverse();
+    return props.battleLog.players;
+  }, [shouldReversePlayers, props.battleLog.players]);
+
   return (
     <Link href={`/live-log/${props.battleLog.id}`}>
-      <Card>
+      <Card className={cn(
+        props.battleLog.winner === props.currentUserScreenName && 'bg-green-100',
+        props.battleLog.winner !== props.currentUserScreenName && 'bg-red-100',
+      )}>
         <CardHeader>
           <div className="flex items-center">
-            <Sprite name={props.battleLog.players[0].deck} />
+            <Sprite name={players[0].deck} />
             <div className="font-semibold ml-2 mr-3">vs</div>
-            <Sprite name={props.battleLog.players[1].deck} />
+            <Sprite name={players[1].deck} />
           </div>
           {/* <CardTitle>game</CardTitle> */}
           <CardDescription>{formatDistanceToNowStrict(props.battleLog.date)} ago</CardDescription>
--- components/battle-logs/MyBattleLogPreviews.tsx
+++ components/battle-logs/MyBattleLogPreviews.tsx
@@ -2,19 +2,23 @@ import { createClient } from "@/utils/supabase/server";
 import { User } from "@supabase/supabase-js";
 import { BattleLogPreview } from "./BattleLogPreview";
 import { parseBattleLog } from "./battle-log.utils";
+import { fetchUserData } from "../user-data.utils";
 
 interface MyBattleLogPreviewsProps {
   user: User | null;
 }
 
 export async function MyBattleLogPreviews (props: MyBattleLogPreviewsProps) {
+  if (!props.user) return; 
+
   const supabase = createClient();
-  const { data: logData } = await supabase.from('logs').select('id,created_at,log').eq('user', props.user?.id).order('created_at', { ascending: false });
+  const { data: logData } = await supabase.from('logs').select('id,created_at,log').eq('user', props.user.id).order('created_at', { ascending: false });
+  const userData = await fetchUserData(props.user.id);
 
   return (
     <div className="flex flex-col gap-2">
       {logData?.map((battleLog) => (
-        <BattleLogPreview battleLog={parseBattleLog(battleLog.log, battleLog.id, battleLog.created_at)} />
+        <BattleLogPreview battleLog={parseBattleLog(battleLog.log, battleLog.id, battleLog.created_at)} currentUserScreenName={userData?.live_screen_name} />
       ))}
     </div>
   )
