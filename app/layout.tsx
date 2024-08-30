715f696fa1153cc8d70fdd708cc56bdc855801de
Jared Grimes <jaredlgrimes@hotmail.com>
2024-08-30 00:15:14 -0500

    changed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Buddy Poffin",
+  description: "Your favorite PTCG testing companion.",
 };
 
 export default function RootLayout({
