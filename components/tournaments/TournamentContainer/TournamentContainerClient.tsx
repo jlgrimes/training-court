1de8f6926bec9e7136fede1cf2a185bb89bf12e3
jw-kriewall <jwkriewall@gmail.com>
2024-09-03 15:19:02 -0400

    Updates the user's record of their Regional. Fade in / fade out set when record changes. No need to refresh the UI to show new record.

--- components/tournaments/TournamentContainer/TournamentContainerClient.tsx
+++ components/tournaments/TournamentContainer/TournamentContainerClient.tsx
@@ -17,10 +17,15 @@ interface TournamentContainerClientProps {
 
 export const TournamentContainerClient = (props: TournamentContainerClientProps) => {
   const [rounds, setRounds] = useState(props.rounds);
+  const [isUpdating, setIsUpdating] = useState(false);
 
   const updateClientRoundsOnAdd = useCallback((newRound: Database['public']['Tables']['tournament rounds']['Row']) => {
-    setRounds([...rounds, newRound]);
-  }, [setRounds, rounds]);
+    setIsUpdating(true);
+    setTimeout(() => {
+      setRounds((prevRounds) => [...prevRounds, newRound]);
+      setIsUpdating(false);
+    }, 500);
+  }, []);
 
   return (
     <div className="flex-1 flex flex-col w-full h-full p-8 sm:max-w-xl justify-between gap-2">
@@ -31,13 +36,20 @@ export const TournamentContainerClient = (props: TournamentContainerClientProps)
             <h3 className="text-sm text-muted-foreground">{displayTournamentDate(props.tournament.date_from, props.tournament.date_to)}</h3>
           </div>
           <EditableTournamentArchetype tournament={props.tournament} />
-          <h2 className="text-xl font-semibold tracking-wider">{getRecord(props.rounds)}</h2>
+          <h2 className={`text-xl font-semibold tracking-wider transition-opacity duration-500 ${isUpdating ? 'opacity-0' : 'opacity-100'}`}>{getRecord(rounds)}</h2>
         </div>
         
         <div className="flex flex-col gap-4">
           <div className="flex flex-col gap-4">
             <TournamentRoundList rounds={rounds} />
-            {props.user?.id && (props.user.id === props.tournament.user) && <AddTournamentRound tournamentId={props.tournament.id} userId={props.user.id} roundsLength={rounds.length} updateClientRoundsOnAdd={updateClientRoundsOnAdd} />}
+            {props.user?.id && (props.user.id === props.tournament.user) && (
+              <AddTournamentRound
+                tournamentId={props.tournament.id}
+                userId={props.user.id}
+                roundsLength={rounds.length}
+                updateClientRoundsOnAdd={updateClientRoundsOnAdd}
+              />
+            )}
           </div>
         </div>
       </div>
