3609ca901739cb5590852fa2cb688d4aae4612cb
Jared Grimes <jaredlgrimes@hotmail.com>
2024-09-11 15:34:10 -0500

    add default view for tournament

--- components/tournaments/Preview/MyTournamentPreviews.tsx
+++ components/tournaments/Preview/MyTournamentPreviews.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import { User } from "@supabase/supabase-js";
 import TournamentPreview from "./TournamentPreview";
+import { Card, CardDescription, CardHeader } from "@/components/ui/card";
 
 interface MyTournamentPreviewsProps {
   user: User | null;
@@ -9,6 +10,17 @@ interface MyTournamentPreviewsProps {
 export async function MyTournamentPreviews (props: MyTournamentPreviewsProps) {
   const supabase = createClient();
   const { data: tournamentData } = await supabase.from('tournaments').select('*').eq('user', props.user?.id).order('date_from', { ascending: false });
+
+  if (tournamentData && tournamentData?.length === 0) {
+    return (
+      <Card className="border-none">
+        <CardHeader className="px-2">
+          <CardDescription>You can add tournaments from the past, present, or future.</CardDescription>
+          <CardDescription>Click Add Tournament to get started!</CardDescription>
+        </CardHeader>
+      </Card>
+    )
+  }
 
   return (
     <div className="flex flex-col gap-2">
