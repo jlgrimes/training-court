e333c0287cee94bc2cffdf412f9fe9f07f717bef
Jared Grimes <jaredlgrimes@hotmail.com>
2024-09-11 17:18:38 -0500

    added badges to tournaments

--- components/tournaments/Preview/TournamentPreview.tsx
+++ components/tournaments/Preview/TournamentPreview.tsx
@@ -1,11 +1,13 @@
 
 import Link from "next/link";
+import { Badge } from "@/components/ui/badge"
 import { Card, CardDescription, CardTitle, SmallCardHeader } from "../../ui/card";
 import { displayTournamentDate, getRecord } from "../utils/tournaments.utils";
-import { Sprite } from "../../archetype/Sprite";
 import { fetchRounds } from "../utils/tournaments.server.utils";
 import { EditableTournamentArchetype } from "@/components/archetype/AddArchetype/AddTournamentArchetype";
 import { Database } from "@/database.types";
+import { formatDistanceToNowStrict, isAfter, isBefore } from "date-fns";
+import { RadioTower, Watch } from "lucide-react";
 
 interface TournamentPreviewProps {
   tournament: Database['public']['Tables']['tournaments']['Row'];
@@ -26,6 +28,14 @@ export default async function TournamentPreview(props: TournamentPreviewProps) {
             <CardDescription className="grid gap-4">
               {displayTournamentDate(props.tournament.date_from, props.tournament.date_to)}
             </CardDescription>
+            {isBefore(new Date(), props.tournament.date_from) && (
+              <Badge className="mt-2 bg-purple-100" variant='secondary'><Watch className="h-4 w-4 mr-1" /> Live in {formatDistanceToNowStrict(props.tournament.date_from, {
+                roundingMethod: 'ceil',
+              })}</Badge>
+            )}
+            {isAfter(new Date(), props.tournament.date_from) && isBefore(new Date(), props.tournament.date_to) && (
+              <Badge className="mt-2 bg-green-100" variant='secondary'><RadioTower className="h-4 w-4 mr-1" /> Live</Badge>
+            )}
           </div>
           <CardTitle className="text-right">{rounds && getRecord(rounds)}</CardTitle>
         </SmallCardHeader>
