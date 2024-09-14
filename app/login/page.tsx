52d4e5a9a3d3e2d06cfd67d8941249b964928bad
Jared Grimes <jaredlgrimes@hotmail.com>
2024-09-13 21:16:24 -0400

    redirects to home on sign up

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -52,7 +52,7 @@ export default function Login({
       return redirect("/login?message=Could not authenticate user");
     }
 
-    return redirect("/login?message=Check email to continue sign in process");
+    return redirect("/home");
   };
 
   return (
