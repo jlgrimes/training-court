9aefba25ff0aac91d33204890e9d4c724503b68f
Jared Grimes <jaredlgrimes@hotmail.com>
2024-09-19 13:16:42 -0500

    made admin tabs

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,6 +6,7 @@ import { Label } from "@/components/ui/label";
 import { fetchAvatarImages } from "@/components/avatar/avatar.server.utils";
 import { getMainSelectableAvatars } from "@/components/avatar/avatar.utils";
 import { CardDescription, CardTitle } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default async function AdminPage() {
   const user = await fetchCurrentUser();
@@ -22,18 +23,32 @@ export default async function AdminPage() {
   return (
     <div className="flex flex-col py-4 lg:py-8 px-8 lg:px-16 gap-4 w-full h-full">
       <CardTitle>Welcome admin!</CardTitle>
-      <Label>Most commonly used avatars:</Label>
-      <div className="grid grid-cols-12 gap-2">
-        {mostCommonlyUsedAvatars?.map(({ avatar, avatar_count }) => (
-          <div key={`${avatar}-count`} className="flex flex-col items-center">
-            <img className="pixel-image" src={avatar} />
-            <Label>{avatar_count}</Label>
-          </div>
-        ))}
-      </div>
-      <Label>Unused avatars:</Label>
-      {(unusedAvatars.length === 0) && <CardDescription>All avatars are being used!</CardDescription>}
-      {unusedAvatars.map((avatar) => <img className="pixel-image" src={avatar} />)}
+
+      <Tabs defaultValue="feedback">
+        <TabsList>
+          <TabsTrigger value="feedback">Feedback</TabsTrigger>
+          <TabsTrigger value="avatars">Avatars</TabsTrigger>
+        </TabsList>
+        <TabsContent value="avatars">
+          <>
+          <Label>Most commonly used avatars:</Label>
+             <div className="grid grid-cols-12 gap-2">
+              {mostCommonlyUsedAvatars?.map(({ avatar, avatar_count }) => (
+                <div key={`${avatar}-count`} className="flex flex-col items-center">
+                  <img className="pixel-image" src={avatar} />
+                  <Label>{avatar_count}</Label>
+                </div>
+              ))}
+            </div>
+            <Label>Unused avatars:</Label>
+            {(unusedAvatars.length === 0) && <CardDescription>All avatars are being used!</CardDescription>}
+            {unusedAvatars.map((avatar) => <img className="pixel-image" src={avatar} />)}
+          </>
+        </TabsContent>
+        <TabsContent value="feedback">
+          WIP
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
