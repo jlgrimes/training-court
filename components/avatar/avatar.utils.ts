82299df900fecd0e6ed81e2828a6cb8cb8bc8a15
Jared Grimes <jaredlgrimes@hotmail.com>
2024-09-22 16:11:10 -0500

    bunny

--- components/avatar/avatar.utils.ts
+++ components/avatar/avatar.utils.ts
@@ -1,8 +1,9 @@
+import { bunnyUserId } from "../premium/premium.utils";
+
 export const getAvatarSrc = (fileName: string) => `/assets/trainers/${fileName}`;
 
 export const getMainSelectableAvatars = (images: string[], userId: string) => {
   let exclusiveAvatars = ['ace trainer', 'cynthia', 'bunny', 'pokemon-center-lady', 'N.png', 'ghetsis', 'riley'];
-  const bunnyUserId = 'f0a37b75-3ecb-4aaa-a5ac-ecb679685ed2';
 
   if (userId === bunnyUserId) {
     exclusiveAvatars = exclusiveAvatars.filter((avatar) => avatar !== 'bunny');
--- components/premium/premium.utils.ts
+++ components/premium/premium.utils.ts
@@ -1,6 +1,8 @@
 import { User } from "@supabase/supabase-js";
 import { isUserAnAdmin } from "../admin/admin.utils";
 
+export const bunnyUserId = 'f0a37b75-3ecb-4aaa-a5ac-ecb679685ed2';
+
 export function isPremiumUser(user: User | null) {
-  return isUserAnAdmin(user);
+  return (user?.id === bunnyUserId) || isUserAnAdmin(user);
 }
