4f99b64d0fd8c631d58074a365d04065543552c9
Jared Grimes <jaredlgrimes@hotmail.com>
2024-10-01 17:35:04 -0500

    fixed compile err

--- components/friends/friend-requests/FriendRequestAcceptPage.tsx
+++ components/friends/friend-requests/FriendRequestAcceptPage.tsx
@@ -3,7 +3,7 @@
 import { getAvatarSrc } from "@/components/avatar/avatar.utils";
 import { AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import { Label } from "@/components/ui/label"
 import { Database } from "@/database.types"
 import { redirect } from "next/navigation"
@@ -17,22 +17,22 @@ interface FriendRequestAcceptPageProps {
 
 export const FriendRequestAcceptPage = (props: FriendRequestAcceptPageProps) => {
   return (
-    <div className="flex-1 flex flex-col w-full h-full sm:max-w-lg justify-between gap-2 p-4">
-      <Card>
-        <CardContent>
-        <div>
+    <div className="flex-1 flex flex-col w-full h-full sm:max-w-lg justify-between items-center gap-2 p-4">
+      <Card className="py-2">
+        <CardHeader>
+        <div className="flex items-center gap-2">
           {props.friendRequestData ? (
-            <div className='flex gap-1'>
-              {props.friendRequestData.user_sending.avatar && <AvatarImage className="pixel-image" src={getAvatarSrc(props.friendRequestData.user_sending.avatar)} />}
+            <div className='flex gap-1 items-center'>
+              {props.friendRequestData.user_sending.avatar && <img className="h-10 w-10 pixel-image" src={getAvatarSrc(props.friendRequestData.user_sending.avatar)} />}
               <strong>{props.friendRequestData.user_sending.live_screen_name}</strong>
             </div>
           ) : <strong>Someone</strong>}
           sent you a friend request.
         </div>
-        </CardContent>
-        <CardFooter>
+        </CardHeader>
+        <CardFooter className="gap-2 justify-center">
           <Button onClick={() => addFriend(props.friendRequestData, props.accepterUserId)}>Accept</Button>
-          <Button variant='secondary' onClick={() => redirect("/")}>Nah</Button>
+          <Button variant='secondary' onClick={() => window.location.href = '/home'}>Nah</Button>
         </CardFooter>
       </Card>
     </div>
