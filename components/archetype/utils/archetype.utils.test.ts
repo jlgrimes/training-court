8fac81f104046a616f3ad749cffb0b8c5b91441f
Jared Grimes <jaredlgrimes@hotmail.com>
2024-10-07 17:18:33 -0500

    fixed test names

--- components/archetype/utils/archetype.utils.test.ts
+++ components/archetype/utils/archetype.utils.test.ts
@@ -8,14 +8,14 @@ describe('archetype utils', () => {
     expect(determineArchetype(mockLog, 'comp0cker', 'en')).toEqual('regidrago');
   });
 
-  it('should correctly determine archetype from evolving', () => {
+  it('should correctly determine archetype in German', () => {
     const mockLog = [
       `comp0cker hat Ur-Palkia-V auf der Bank zu Ur-Palkia-VSTAR entwickelt.`,
     ];
     expect(determineArchetype(mockLog, 'comp0cker', 'de')).toEqual('palkia-origin');
   });
 
-  it('should correctly determine archetype in German', () => {
+  it('should correctly determine archetype from evolving', () => {
     const mockLog = [
       `- comp0cker evolved into Regidrago VSTAR`,
     ];
