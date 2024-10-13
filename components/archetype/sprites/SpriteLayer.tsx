62701a2ca5d65329711d44a3458df7c3c0518539
jw-kriewall <jwkriewall@gmail.com>
2024-10-13 00:16:42 -0400

    Pushing

--- components/archetype/sprites/SpriteLayer.tsx
+++ components/archetype/sprites/SpriteLayer.tsx
@@ -5,11 +5,17 @@ interface SpriteLayerProps {
 }
 
 export const SpriteLayer = (props: SpriteLayerProps) => {
+  const decks = props.decks.filter((deck) => deck);
+
+  //@TODO: How can we make this look nicer? Maybe a divider icon?
   return (
     <div className="flex items-center">
-      {props.decks.filter((deck) => deck).map((deck) => (
-        <span key={deck} className="-mr-3"><Sprite name={deck} /></span>
+      {decks.map((deck, index) => (
+        <div key={deck} className="flex items-center">
+          <span className="-mr-3"><Sprite name={deck} /></span>
+          {index < decks.length - 1 && <span className="mx-1"></span>}
+        </div>
       ))}
     </div>
-  )
-}
+  );
+};
