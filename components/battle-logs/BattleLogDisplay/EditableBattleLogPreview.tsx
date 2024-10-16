f0b585c6678bd3f4603763161e17b52bbe01be5a
jw-kriewall <jwkriewall@gmail.com>
2024-10-16 16:11:14 -0400

    Pushing

--- components/battle-logs/BattleLogDisplay/EditableBattleLogPreview.tsx
+++ components/battle-logs/BattleLogDisplay/EditableBattleLogPreview.tsx
@@ -28,7 +28,7 @@ export const EditableBattleLogPreview = (props: EditableBattleLogPreviewProps) =
       )}>
         <BattleLogPreview battleLog={props.battleLog} />
       </div>
-      <BattleLogEditButton isEditing={props.isEditing} log={props.battleLog} currentPlayer={(props.battleLog.players[0].name.toLowerCase() === props.currentUserScreenName?.toLowerCase()) ? props.battleLog.players[0] : props.battleLog.players[1]}
+      <BattleLogEditButton isEditing={props.isEditing} log={props.battleLog} currentPlayer={(props.battleLog.players[0].name.toLowerCase() === userData?.live_screen_name?.toLowerCase()) ? props.battleLog.players[0] : props.battleLog.players[1]}
         shouldDisable={userIsNotInTheBattleLog} />
       <BattleLogDeleteButton isEditing={props.isEditing} logId={props.battleLog.id} />
     </div>
