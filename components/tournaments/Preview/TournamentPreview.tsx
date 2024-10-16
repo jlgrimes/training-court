03e3be7c44b83f45a2a8a48cfdc12b597c6fefa1
Jared Grimes <jaredlgrimes@hotmail.com>
2024-10-16 16:02:04 -0500

    made the previews smaller

--- components/tournaments/Preview/TournamentPreview.tsx
+++ components/tournaments/Preview/TournamentPreview.tsx
@@ -22,33 +22,33 @@ interface TournamentPreviewProps {
   shouldHideCategoryBadge?: boolean;
 }
 
+// badges dead code
+{/* <div className="flex gap-1 flex-col sm:flex-row">
+  {isBefore(new Date(), props.tournament.date_from) && (
+    <Badge className="bg-purple-100" variant='secondary'><Watch className="h-4 w-4 mr-1" /> Live in {formatDistanceToNowStrict(props.tournament.date_from, {
+      roundingMethod: 'ceil',
+      unit: 'day'
+    })}</Badge>
+  )}
+  {isAfter(new Date(), props.tournament.date_from) && isBefore(new Date(), props.tournament.date_to) && (
+    <Badge className="bg-green-100" variant='secondary'><RadioTower className="h-4 w-4 mr-1" /> Live</Badge>
+  )}
+</div> */}
+
 export default async function TournamentPreview(props: TournamentPreviewProps) {
   return (
     <Link href={`/tournaments/${props.tournament.id}`}>
       <Card clickable>
         <SmallCardHeader className="flex ml-2">
-            <div className="flex gap-2 justify-between items-center">
-              <div className="flex flex-col gap-1">
-                <Sprite name={props.tournament.deck} />
-                <div>
-                  <CardTitle className="flex gap-2 items-center">{props.tournament.name}{props.tournament.category && !props.shouldHideCategoryBadge && <TournamentCategoryIcon category={props.tournament.category as TournamentCategory} />}</CardTitle>
-                  <CardDescription className="grid gap-4">
-                    {displayTournamentDate(props.tournament.date_from, props.tournament.date_to)}
-                  </CardDescription>
-                </div>
-                <div className="flex gap-1 flex-col sm:flex-row">
-                  {isBefore(new Date(), props.tournament.date_from) && (
-                    <Badge className="bg-purple-100" variant='secondary'><Watch className="h-4 w-4 mr-1" /> Live in {formatDistanceToNowStrict(props.tournament.date_from, {
-                      roundingMethod: 'ceil',
-                      unit: 'day'
-                    })}</Badge>
-                  )}
-                  {isAfter(new Date(), props.tournament.date_from) && isBefore(new Date(), props.tournament.date_to) && (
-                    <Badge className="bg-green-100" variant='secondary'><RadioTower className="h-4 w-4 mr-1" /> Live</Badge>
-                  )}
-                </div>
+            <div className="grid grid-cols-4 items-center">
+              <Sprite name={props.tournament.deck} />
+              <div className="text-left col-span-2">
+                <CardTitle className="flex gap-2 items-center">{props.tournament.name}</CardTitle>
+                <CardDescription className="grid gap-4">
+                  {displayTournamentDate(props.tournament.date_from, props.tournament.date_to)}
+                </CardDescription>
               </div>
-              <div className="flex flex-col items-end gap-1">
+              <div className="flex flex-col items-end">
                 <CardTitle className="text-right whitespace-nowrap text-lg">{props.rounds && getRecord(props.rounds)}</CardTitle>
                 {props.tournament.placement && <Label className="text-muted-foreground">{renderTournamentPlacement(props.tournament.placement as TournamentPlacement)}</Label>}
               </div>
