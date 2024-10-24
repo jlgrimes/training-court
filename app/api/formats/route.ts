0530af755d5f01265939f17744cb66482076c713
Jared Grimes <jaredlgrimes@hotmail.com>
2024-10-24 17:34:32 -0500

    gets a formats route

---
+++ app/api/formats/route.ts
@@ -0,0 +1,26 @@
+import { parseISO, nextFriday, add } from "date-fns";
+
+export async function GET() {
+  try {
+    const response = await fetch('https://api.pokemontcg.io/v2/sets');
+    const data: Record<string, Record<string, any>[]> = await response.json();
+    const alteredData = data['data'].map((obj) => {
+      const releaseDate = parseISO(obj['releaseDate'].replaceAll('/', ''));
+      const legalityDate = add(nextFriday(releaseDate), {
+        weeks: 1
+      });
+
+      return ({
+        ...obj,
+        releaseDate,
+        legalityDate
+      })
+    })
+
+    return Response.json({ data: alteredData, code: 200 })
+  } catch (error) {
+    console.error(error);
+
+    return Response.json({ message: 'Error scraping', code: 500 })
+  }
+}
