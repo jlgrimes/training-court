f591fa5a18bc5a4a9c9ce888d59ce831ead563e4
Jared Grimes <jaredlgrimes@hotmail.com>
2024-10-24 18:58:03 -0500

    blocks api done

---
+++ app/api/formats/_utils.ts
@@ -0,0 +1,27 @@
+import { isAfter, isBefore } from "date-fns";
+import { PokemonSet } from "./_types";
+
+const rotations = [
+  new Date('2024-04-05'),
+  new Date('2023-04-13'),
+  new Date('2021-09-10'),
+  new Date('2020-04-27'),
+];
+
+export const getRotationBlocks = (sets: PokemonSet[]) => {
+  const setsInRotations = sets.filter((set) => isAfter(set.legalityDate, new Date('2020-04-27')));
+  const rotationStartingSets = rotations.map((startingDate: Date) => {
+    const setRightBeforeRotationIdx = sets.findIndex((set) => isBefore(set.legalityDate, startingDate));
+    return sets[setRightBeforeRotationIdx - 1];
+  });
+
+  return setsInRotations.map((set) => {
+    const startingSetIdx = rotations.findIndex((rotationDate) => isBefore(rotationDate, set.legalityDate)) + 1;
+    if (startingSetIdx >= rotations.length) return null;
+    
+    return {
+      format: `${rotationStartingSets[startingSetIdx].ptcgoCode}-${set.ptcgoCode}`,
+      startDate: set.legalityDate,
+    }
+  }).filter((set) => set);
+}
--- app/api/formats/route.ts
+++ app/api/formats/route.ts
@@ -1,11 +1,12 @@
 import { parseISO, nextFriday, add, isBefore, isAfter } from "date-fns";
 import { PokemonTCGApiPokemonSet } from "./_types";
+import { getRotationBlocks } from "./_utils";
 
 export async function GET() {
   try {
     const response = await fetch('https://api.pokemontcg.io/v2/sets');
     const data: Record<string, PokemonTCGApiPokemonSet[]> = await response.json();
-    const alteredData = data['data'].map((obj) => {
+    const dataWithLegalities = data['data'].map((obj) => {
       const releaseDate = parseISO(obj['releaseDate'].replaceAll('/', ''));
       const legalityDate = add(nextFriday(releaseDate), {
         weeks: 1
@@ -13,16 +14,24 @@ export async function GET() {
 
       return ({
         ...obj,
-        releaseDate,
-        legalityDate
+        releaseDate: releaseDate.toString(),
+        legalityDate: legalityDate.toString()
       })
-    }).sort((a, b) => {
+    })
+    
+    const alteredData = dataWithLegalities.sort((a, b) => {
       if (isBefore(a.releaseDate, b.releaseDate)) return 1;
       if (isAfter(a.releaseDate, b.releaseDate)) return -1;
-      return 0;
-    })
 
-    return Response.json({ data: alteredData, code: 200 })
+      if (a.name.length < b.name.length) return -1;
+      if (a.name.length > b.name.length) return 1;
+
+      return 0;
+    }).filter((set, idx) => dataWithLegalities.findIndex(({ legalityDate }) => legalityDate === set.legalityDate) === idx && !['sve'].includes(set.id) && !set.name.includes(' Promos') && set.series !== 'Other');
+
+    const blocks = getRotationBlocks(alteredData)
+
+    return Response.json({ data: blocks, code: 200 })
   } catch (error) {
     console.error(error);
 
