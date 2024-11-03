5769e34e7afdb3d722d9a0be8f10fce9e6c86732
Jared Grimes <jaredlancegrimes@gmail.com>
2024-11-03 17:22:04 -0600

    made pocket look nice, made sidebar better

--- app/pocket/page.tsx
+++ app/pocket/page.tsx
@@ -13,11 +13,14 @@ export default async function Pocket() {
 
   return (
     <div className="flex flex-col py-4 pl-8 pr-6 gap-6 w-full h-full">
-      <h1 className="text-xl tracking-wide font-semibold text-slate-800">Pocket Games</h1>
-      <div className="flex flex-col gap-4 items-start">
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
+        <div>
+          <h1 className="text-xl tracking-wide font-semibold text-slate-800">Pocket Games</h1>
+          <p className="text-md text-muted-foreground mt-1">Record your games from PTCG Pocket.</p>
+        </div>
         <AddPocketMatch userId={user.id} />
-        <PocketMatchesList userId={user.id} />
       </div>
+      <PocketMatchesList userId={user.id} />
     </div>
   );
 }
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -12,12 +12,14 @@ import {
   SidebarSeparator,
 } from "@/components/ui/sidebar"
 
-import { Calendar, Home, Inbox, Info, ScrollText, Search, Settings, Trophy, WalletMinimal } from "lucide-react"
+import { Calendar, ChevronUp, Home, Inbox, Info, LogIn, ScrollText, Search, Settings, ToggleLeft, ToggleRight, Trophy, WalletMinimal } from "lucide-react"
 import Image from "next/image"
 import { fetchCurrentUser } from "./auth.utils";
 import { ReportBugDialog } from "./app-bar/ReportBugDialog";
 import Link from "next/link";
-import { Badge } from "./ui/badge";
+import { MyProfileAvatar } from "./app-bar/MyProfileAvatar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { LogOutButton } from "./app-bar/LogOutButton";
  
 const items = [
   {
@@ -74,6 +76,18 @@ export async function AppSidebar() {
                 <ReportBugDialog user={user} />
               </SidebarMenuItem>
             )}
+                      {!user && (
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href='/login'>
+                    <LogIn />
+                    <span>Log In</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          )}
             </SidebarMenu>
       </SidebarHeader>
       {user && (
@@ -114,6 +128,33 @@ export async function AppSidebar() {
           </SidebarGroup>
         </SidebarContent>
       )}
+        <SidebarFooter>
+          {user && (
+            <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton>
+                  <MyProfileAvatar user={user} /> {user?.email}
+                    <ChevronUp className="ml-auto" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  side="top"
+                  className="w-[--radix-popper-anchor-width]"
+                >
+                  <DropdownMenuItem asChild>
+                    <a href='/preferences'>
+                      Preferences
+                    </a>
+                  </DropdownMenuItem>
+                  <LogOutButton />
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+          )}
+        </SidebarFooter>
     </Sidebar>
   )
 }
