60a3565f4d863038bf571f7179bf57eea66edef6
jw-kriewall <jwkriewall@gmail.com>
2024-11-12 13:41:50 -0500

    password reset

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -4,6 +4,7 @@ import { SubmitButton } from "./submit-button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { headers } from "next/headers";
 
 export default function ForgotPassword({ searchParams }: { searchParams: { message?: string } }) {
     
@@ -11,7 +12,9 @@ export default function ForgotPassword({ searchParams }: { searchParams: { messa
     "use server";
         
     const supabase = createClient();
-    const defaultUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+    const host = headers().get("host");
+    const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
+    const defaultUrl = `${protocol}://${host}`;
     const email = formData.get("email") as string;
 
     const { error } = await supabase.auth.resetPasswordForEmail(email, {
