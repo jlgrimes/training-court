855e9cd0421338ff0c05b4df295c479d23d3bd28
Jared Grimes <jaredlancegrimes@gmail.com>
2024-12-20 09:09:57 -0600

    cleaned up matches

--- components/pocket/PocketMatchesList.tsx
+++ components/pocket/PocketMatchesList.tsx
@@ -1,55 +1,124 @@
 'use client';
 
-import { usePocketGames } from "@/hooks/pocket/usePocketGames"
-import { Card } from "../ui/card";
-import { Sprite } from "../archetype/sprites/Sprite";
-import { cn } from "@/lib/utils";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "../ui/dropdown-menu";
-import { useCallback } from "react";
-import { createClient } from "@/utils/supabase/client";
-import { useToast } from "../ui/use-toast";
-import { useSWRConfig } from "swr";
+import { usePocketGames } from '@/hooks/pocket/usePocketGames';
+import { Card } from '../ui/card';
+import { Sprite } from '../archetype/sprites/Sprite';
+import { cn } from '@/lib/utils';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { useCallback } from 'react';
+import { createClient } from '@/utils/supabase/client';
+import { useToast } from '../ui/use-toast';
+import { useSWRConfig } from 'swr';
 
-export const PocketMatchesList = ({ userId }: { userId: string | undefined }) => {
+export const PocketMatchesList = ({
+  userId,
+}: {
+  userId: string | undefined;
+}) => {
   const { toast } = useToast();
   const { mutate } = useSWRConfig();
   const { data: games } = usePocketGames(userId);
 
-  const handleDeletePocketGame = useCallback(async (gameId: number) => {
-    const supabase = createClient();
-    const { data, error } = await supabase.from('pocket_games').delete().eq('id', gameId).select();
+  const handleDeletePocketGame = useCallback(
+    async (gameId: number) => {
+      const supabase = createClient();
+      const { data, error } = await supabase
+        .from('pocket_games')
+        .delete()
+        .eq('id', gameId)
+        .select();
 
-    if (error) {
-      toast({
-        variant: "destructive",
-        title: "Uh oh! Something went wrong.",
-        description: error.message,
-      });
-    } else {
-      mutate(['pocket-games', userId], data);
-    }
-  }, [userId]);
+      if (error) {
+        toast({
+          variant: 'destructive',
+          title: 'Uh oh! Something went wrong.',
+          description: error.message,
+        });
+      } else {
+        mutate(['pocket-games', userId], data);
+      }
+    },
+    [userId]
+  );
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 w-full">
-      {games?.map((game) => (
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <Card result={game.result as 'W' | 'L'} className="px-4 py-2 flex justify-between items-center">
-              <Sprite name={game.deck} shouldFill/>
-              <div className={cn(
-                "text-md font-bold",
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className='pl-4'>My deck</TableHead>
+          <TableHead className='pl-3'>Opponent's deck</TableHead>
+          <TableHead className='w-[40px] text-right'>Result</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {games?.map((game, idx) => (
+          <TableRow key={`pocket-game-${idx}`} result={game.result}>
+            <TableCell>
+              <Sprite name={game.deck} />
+            </TableCell>
+            <TableCell>
+              <Sprite name={game.opp_deck} />
+            </TableCell>
+            <TableCell
+              className={cn(
+                'w-[40px] font-bold text-right',
                 game.result === 'W' && 'text-emerald-600',
                 game.result === 'L' && 'text-red-600'
-              )}>{game.result}</div>
-              <Sprite name={game.opp_deck} shouldFill/>
+              )}
+            >
+              {game.result}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 w-full'>
+      {games?.map(game => (
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Card
+              result={game.result as 'W' | 'L'}
+              className='px-4 py-2 flex justify-between items-center'
+            >
+              <Sprite name={game.deck} shouldFill />
+              <div
+                className={cn(
+                  'text-md font-bold',
+                  game.result === 'W' && 'text-emerald-600',
+                  game.result === 'L' && 'text-red-600'
+                )}
+              >
+                {game.result}
+              </div>
+              <Sprite name={game.opp_deck} shouldFill />
             </Card>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuItem onClick={() => handleDeletePocketGame(game.id)}>Delete</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => handleDeletePocketGame(game.id)}>
+              Delete
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       ))}
     </div>
-  )
-}
+  );
+};
--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -1,28 +1,28 @@
-import * as React from "react"
+import * as React from 'react';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
 const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
+  <div className='relative w-full overflow-auto'>
     <table
       ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
+      className={cn('w-full caption-bottom text-sm', className)}
       {...props}
     />
   </div>
-))
-Table.displayName = "Table"
+));
+Table.displayName = 'Table';
 
 const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
-))
-TableHeader.displayName = "TableHeader"
+  <thead ref={ref} className={cn('[&_tr]:border-b', className)} {...props} />
+));
+TableHeader.displayName = 'TableHeader';
 
 const TableBody = React.forwardRef<
   HTMLTableSectionElement,
@@ -30,11 +30,11 @@ const TableBody = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <tbody
     ref={ref}
-    className={cn("[&_tr:last-child]:border-0", className)}
+    className={cn('[&_tr:last-child]:border-0', className)}
     {...props}
   />
-))
-TableBody.displayName = "TableBody"
+));
+TableBody.displayName = 'TableBody';
 
 const TableFooter = React.forwardRef<
   HTMLTableSectionElement,
@@ -43,28 +43,36 @@ const TableFooter = React.forwardRef<
   <tfoot
     ref={ref}
     className={cn(
-      "border-t bg-muted/50 font-medium [&>tr]:last:border-b-0",
+      'border-t bg-muted/50 font-medium [&>tr]:last:border-b-0',
       className
     )}
     {...props}
   />
-))
-TableFooter.displayName = "TableFooter"
+));
+TableFooter.displayName = 'TableFooter';
 
-const TableRow = React.forwardRef<
-  HTMLTableRowElement,
-  React.HTMLAttributes<HTMLTableRowElement>
->(({ className, ...props }, ref) => (
-  <tr
-    ref={ref}
-    className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
-      className
-    )}
-    {...props}
-  />
-))
-TableRow.displayName = "TableRow"
+interface TableRowProps extends React.HTMLAttributes<HTMLTableRowElement> {
+  result?: string;
+}
+
+const TableRow = React.forwardRef<HTMLTableRowElement, TableRowProps>(
+  ({ className, result, ...props }, ref) => (
+    <tr
+      ref={ref}
+      className={cn(
+        'border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted',
+        result === 'W' &&
+          'bg-emerald-100 hover:bg-emerald-100 md:hover:bg-emerald-200',
+        result === 'T' &&
+          'bg-yellow-100 hover:bg-yellow-100 md:hover:bg-yellow-200',
+        result === 'L' && 'bg-red-100 hover:bg-red-100 md:hover:bg-red-200',
+        className
+      )}
+      {...props}
+    />
+  )
+);
+TableRow.displayName = 'TableRow';
 
 const TableHead = React.forwardRef<
   HTMLTableCellElement,
@@ -73,13 +81,13 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+      'h-12 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0',
       className
     )}
     {...props}
   />
-))
-TableHead.displayName = "TableHead"
+));
+TableHead.displayName = 'TableHead';
 
 const TableCell = React.forwardRef<
   HTMLTableCellElement,
@@ -87,11 +95,14 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("px-3 py-1 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    className={cn(
+      'px-3 py-1 align-middle [&:has([role=checkbox])]:pr-0',
+      className
+    )}
     {...props}
   />
-))
-TableCell.displayName = "TableCell"
+));
+TableCell.displayName = 'TableCell';
 
 const TableCaption = React.forwardRef<
   HTMLTableCaptionElement,
@@ -99,11 +110,11 @@ const TableCaption = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <caption
     ref={ref}
-    className={cn("mt-4 text-sm text-muted-foreground", className)}
+    className={cn('mt-4 text-sm text-muted-foreground', className)}
     {...props}
   />
-))
-TableCaption.displayName = "TableCaption"
+));
+TableCaption.displayName = 'TableCaption';
 
 export {
   Table,
@@ -114,4 +125,4 @@ export {
   TableRow,
   TableCell,
   TableCaption,
-}
+};
