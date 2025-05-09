a3c18cd4d55ea0fadebc8bceb9f5cdaa5acc1537
jw-kriewall <jwkriewall@gmail.com>
2025-05-08 22:11:42 -0400

    Missed an import

--- hooks/logs/usePaginatedLogsByDeck.ts
+++ hooks/logs/usePaginatedLogsByDeck.ts
@@ -2,6 +2,7 @@
 
 import useSWR from 'swr';
 import { Database } from '@/database.types';
+import { fetchPaginatedLogsByDistinctDays } from './useLiveLogs.utils';
 
 export function usePaginatedLogsByDeck(userId: string | undefined, page: number, daysPerPage: number = 4) {
   const { data, error, isLoading } = useSWR<Database['public']['Tables']['logs']['Row'][] | undefined>(
