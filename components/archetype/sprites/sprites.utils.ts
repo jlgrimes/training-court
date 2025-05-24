48021ef2e27aae24b91f6337f4c0f9d2fb32ad07
John Kriewall <109815141+jw-kriewall@users.noreply.github.com>
2025-05-24 13:22:41 -0400

    Update sprites.utils.ts
    See comment in the code for more details. Changing hard coded url value to r2.limitlesstcg.net.

--- components/archetype/sprites/sprites.utils.ts
+++ components/archetype/sprites/sprites.utils.ts
@@ -6,7 +6,12 @@ export const fetchLimitlessSprites = async (): Promise<string[]> => {
   return json['pokedex'] as string[];
 };
 
-export const pkmnToImgSrc = (pkmn: string | null | undefined) => (pkmn) ? `https://limitlesstcg.s3.us-east-2.amazonaws.com/pokemon/gen9/${pkmn}.png` : undefined;
+// The computer I am running this off of doesn't have enough juice to set up VSCode. I am commenting out the line below which contains the hardcoded value https://limitlesstcg.s3.us-east-2.amazonaws.com
+// https://limitlesstcg.s3.us-east-2.amazonaws.com was the original value. Not sure why it is not working now, but I receive a 403 Unauthorized when using it.
+// Changing the hardcoded url to r2.limitlesstcg.net. We should look into becoming unreliant on limitless for these images.
+//export const pkmnToImgSrc = (pkmn: string | null | undefined) => (pkmn) ? `https://limitlesstcg.s3.us-east-2.amazonaws.com/pokemon/gen9/${pkmn}.png` : undefined;
+export const pkmnToImgSrc = (pkmn: string | null | undefined) => (pkmn) ? `https://r2.limitlesstcg.net/pokemon/gen9/${pkmn}.png` : undefined;
+
 export const imgSrcToPkmnName = (src: string) => {
   const splitSrc = src.split('/');
   return splitSrc[splitSrc.length - 1].split('.png')[0]
