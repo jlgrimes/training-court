2c555e4a4fbe3427cd735d0f217da08a151c8dc4
jw-kriewall <jwkriewall@gmail.com>
2025-07-22 13:27:10 -0400

    Pushing log format update

--- components/tournaments/Format/tournament-format.types.ts
+++ components/tournaments/Format/tournament-format.types.ts
@@ -1,9 +1,9 @@
 // Playable formats differ between tournaments and logs. 
 // Since PTCGL doesn't support alternate formats like GLC (yet), the array for log formats will always be smaller than for tournaments
 
-export type LogFormats = 'SVI-DRI' | 'SVI-JTG' | 'BRS-PRE' | 'BRS-SSP' | 'BRS-SCR';
+export type LogFormats = 'SVI-WHT/BLK' | 'SVI-DRI' | 'SVI-JTG' | 'BRS-PRE' | 'BRS-SSP' | 'BRS-SCR';
 export type LogFormatsTab = LogFormats | 'All';
-export const logFormats: LogFormats[] = ['SVI-DRI', 'SVI-JTG', 'BRS-PRE', 'BRS-SSP', 'BRS-SCR']
+export const logFormats: LogFormats[] = ['SVI-WHT/BLK', 'SVI-DRI', 'SVI-JTG', 'BRS-PRE', 'BRS-SSP', 'BRS-SCR']
 
 export type TournamentFormats = 'SVI-WHT/BLK' | 'SVI-DRI' | 'SVI-JTG' | 'BRS-PRE' | 'BRS-SSP' | 'BRS-SCR' | 'PTCG Pocket' | 'GLC' ;
 export type TournamentFormatsTab = TournamentFormats | 'All';
