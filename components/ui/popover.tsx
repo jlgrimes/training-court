5395a27552635fef43a519cfd5653cb256136155
jw-kriewall <jwkriewall@gmail.com>
2025-08-13 09:49:57 -0400

    Removed comments

--- components/ui/popover.tsx
+++ components/ui/popover.tsx
@@ -5,7 +5,7 @@ import * as PopoverPrimitive from "@radix-ui/react-popover"
 
 import { cn } from "@/lib/utils"
 
-const Popover = PopoverPrimitive.Root
+const  Popover = PopoverPrimitive.Root
 
 const PopoverTrigger = PopoverPrimitive.Trigger
 
