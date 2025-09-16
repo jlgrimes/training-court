96c410c046f773e0ea076442c01aaed0f0c8e724
jw-kriewall <jwkriewall@gmail.com>
2025-09-16 14:28:06 -0400

    Removed unused commits

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,11 +1,7 @@
 import { redirect } from 'next/navigation';
 
 import { fetchCurrentUser } from '@/components/auth.utils';
-import { BattleLogsHomePreview } from '@/components/battle-logs/BattleLogsHome/BattleLogsHomePreview';
-import { TournamentsHomePreview } from '@/components/tournaments/TournamentsHome/TournamentsHomePreview';
-import { isPremiumUser } from '@/components/premium/premium.utils';
 import { MatchupsOverview } from '@/components/premium/matchups/MatchupsOverview';
-import { TrainingCourtWelcome } from '@/components/TrainingCourtWelcome';
 
 export default async function Stats() {
   const user = await fetchCurrentUser();
