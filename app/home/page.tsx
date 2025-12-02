57ada1811fb45492db91380272f7ee206c622964
jw-kriewall <jwkriewall@gmail.com>
2025-12-01 21:34:16 -0500

    Pushing an update when users have nothing selected

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,6 +5,7 @@ import { BattleLogsHomePreview } from '@/components/battle-logs/BattleLogsHome/B
 import { TournamentsHomePreview } from '@/components/tournaments/TournamentsHome/TournamentsHomePreview';
 import { TrainingCourtWelcome } from '@/components/TrainingCourtWelcome';
 import { fetchPreferredGames } from '@/components/user-data.utils';
+import { GamePreferences } from '@/components/preferences/GamePreferences';
 import { isGameEnabled } from '@/lib/game-preferences';
 
 export default async function Profile() {
@@ -15,13 +16,23 @@ export default async function Profile() {
   }
 
   const preferredGames = await fetchPreferredGames(user.id);
+  const hasPreferredGames = preferredGames.length > 0;
   const showPokemonTcg = isGameEnabled(preferredGames, 'pokemon-tcg');
 
   return (
     <>
       <TrainingCourtWelcome userId={user.id} />
 
-      {showPokemonTcg && (
+      {!hasPreferredGames && (
+        <div>
+          <GamePreferences
+            userId={user.id}
+            initialPreferredGames={preferredGames}
+          />
+        </div>
+      )}
+
+      {hasPreferredGames && showPokemonTcg && (
         <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
           <BattleLogsHomePreview userId={user.id} />
           <TournamentsHomePreview user={user} />
