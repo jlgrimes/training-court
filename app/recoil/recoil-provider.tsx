6c787b08424d23ac7f3425455fcc71486674e84e
jw-kriewall <jwkriewall@gmail.com>
2025-12-15 22:41:51 -0500

    Pushing updates

--- app/recoil/recoil-provider.tsx
+++ app/recoil/recoil-provider.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { RecoilObserver } from '@theohagos/recoil-observer';
 import { RecoilRoot } from 'recoil';
 
 export function RecoilProvider({ children }: { children: React.ReactNode }) {
-  return <RecoilRoot>
-    {/* <RecoilObserver env="development" /> */}
-    {children}</RecoilRoot>;
+  return (
+    <RecoilRoot>
+      {children}
+    </RecoilRoot>
+  );
 }
