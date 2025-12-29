67fc515d0b6ccdb01851d484087ff03289870baa
Jared Grimes <jaredgrimes@microsoft.com>
2025-12-29 13:52:28 -0600

    Refactor BattleLogsHomePreview and BattleLogsHomePreviewClient components for improved state management and user experience

--- components/battle-logs/BattleLogsHome/BattleLogsHomePreview.tsx
+++ components/battle-logs/BattleLogsHome/BattleLogsHomePreview.tsx
@@ -1,5 +1,7 @@
 import { Header } from "@/components/ui/header";
-import { BattleLogsHomePreviewClient } from "./BattleLogsHomePreviewClient";
+import { AddBattleLogInput } from "../BattleLogInput/AddBattleLogInput";
+import { BattleLogsByDayPreview } from "./BattleLogsByDayPreview";
+import { parseBattleLog } from "../utils/battle-log.utils";
 import { fetchUserDataServer, fetchBattleLogsServer } from "@/lib/server/home-data";
 
 interface BattleLogsHomePreviewProps {
@@ -19,14 +21,22 @@ export async function BattleLogsHomePreview({ userId }: BattleLogsHomePreviewPro
     fetchBattleLogsServer(userId, 0, 4),
   ]);
 
+  const parsedLogs = battleLogs.map(log => (
+    parseBattleLog(log.log, log.id, log.created_at, log.archetype, log.opp_archetype, userData?.live_screen_name ?? null)
+  ));
+
   return (
     <div className="flex flex-col gap-4">
       <Header>PTCG Logs</Header>
-      <BattleLogsHomePreviewClient
-        userId={userId}
-        userData={userData}
-        initialLogs={battleLogs}
-      />
+      <div className="flex flex-col gap-4">
+        {userData?.live_screen_name && parsedLogs.length > 0 && (
+          <BattleLogsByDayPreview
+            userData={userData}
+            battleLogs={parsedLogs}
+          />
+        )}
+        <AddBattleLogInput userData={userData ?? null} />
+      </div>
     </div>
   );
 }
