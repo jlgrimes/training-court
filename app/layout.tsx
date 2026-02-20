ce18145790a3b2f2e05aab27df2c428a65489317
jw-kriewall <jwkriewall@gmail.com>
2026-02-20 16:39:53 -0500

    Pushing ezoic ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,25 @@ export default async function RootLayout({
      <html lang="en" className={`${GeistSans.className} ${isDark ? "dark" : ""}`} suppressHydrationWarning>
       <head>
         <Script
+          data-cfasync="false"
+          src="https://cmp.gatekeeperconsent.com/min.js"
+          strategy="beforeInteractive"
+        />
+        <Script
+          data-cfasync="false"
+          src="https://the.gatekeeperconsent.com/cmp.min.js"
+          strategy="beforeInteractive"
+        />
+        <Script
+          async
+          src="//www.ezojs.com/ezoic/sa.min.js"
+          strategy="beforeInteractive"
+        />
+        <Script id="ezoic-init" strategy="beforeInteractive">
+          {`window.ezstandalone = window.ezstandalone || {};
+ezstandalone.cmd = ezstandalone.cmd || [];`}
+        </Script>
+        <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3757741726375606"
           crossOrigin="anonymous"
